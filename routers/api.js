36d79580f5b95db6ba2264993f1ebfa9b6973aa7
markmarkyesyes <markhahn545@gmail.com>
2017-04-28 17:01:44 -0500

    updating push

--- routers/api.js
+++ routers/api.js
@@ -1,24 +1,14 @@
 const express = require("express");
 const router = express();
 
-
-///////////////////
-//Word generator dependency
-//////////////////
-var WordPOS = require('wordpos');
-let wordpos = new WordPOS();
-
 router.get("/nouns", (req, res, next) => {
-    let count = req.query.count;
-    wordpos.randNoun({count})
-    .then((nouns) => {
-        res.json(nouns);
+  let count = req.query.count;
+  wordpos
+    .randNoun({count})
+    .then(nouns => {
+      res.json(nouns);
     })
     .catch(next);
 });
-
-
-
-
 
 module.exports = router;
