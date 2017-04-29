15d7d3c67cee5e4c82bb5a76449cd729cd0a147a
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-29 04:05:24 +0000

    Add test for madlib endpoint

--- routers/api.js
+++ routers/api.js
@@ -3,72 +3,106 @@ const express = require("express");
 const router = express();
 const wordPOS = require("wordpos");
 const wordpos = new wordPOS();
+const _ = require("lodash");
 
 router.get("/nouns", (req, res, next) => {
-  let count = req.query.count;
-  wordpos
-    .randNoun({count})
-    .then(nouns => {
-      res.json(nouns);
-    })
-    .catch(next);
+    let count = req.query.count;
+    wordpos
+        .randNoun({
+            count
+        })
+        .then(nouns => {
+            res.json(nouns);
+        })
+        .catch(next);
 });
 
 router.get("/verbs", (req, res, next) => {
-  let count = req.query.count;
-  wordpos
-    .randVerb({count})
-    .then(verbs => {
-      res.json(verbs);
-    })
-    .catch(next);
+    let count = req.query.count;
+    wordpos
+        .randVerb({
+            count
+        })
+        .then(verbs => {
+            res.json(verbs);
+        })
+        .catch(next);
 });
 
 router.get("/adjectives", (req, res, next) => {
-  let count = req.query.count;
-  wordpos
-    .randAdjective({count})
-    .then(adjectives => {
-      res.json(adjectives);
-    })
-    .catch(next);
+    let count = req.query.count;
+    wordpos
+        .randAdjective({
+            count
+        })
+        .then(adjectives => {
+            res.json(adjectives);
+        })
+        .catch(next);
 });
 
 router.get("/adverbs", (req, res, next) => {
-  let count = req.query.count;
-  wordpos
-    .randAdverb({count})
-    .then(adverbs => {
-      res.json(adverbs);
-    })
-    .catch(next);
+    let count = req.query.count;
+    wordpos
+        .randAdverb({
+            count
+        })
+        .then(adverbs => {
+            res.json(adverbs);
+        })
+        .catch(next);
 });
 
 router.post("/madlibs", (req, res, next) => {
-  Sentencer.configure({
-    nounList: req.body.nouns,
-    adjectiveList: req.body.adjectives,
-    verbsList: req.body.verbs,
-    adverbsList: req.body.adverbs,
-    actions: {
-      adverb: function() {
-        let i = Math.floor(Math.random() * adverbsList.length);
-        return adverbsList[i];
-      },
-      verb: function() {
-        let i = Math.floor(Math.random() * verbsList.length);
-        return verbsList[i];
-      }
-    }
-  });
-  console.log("in the route", req.body.sentences);
-  Promise.all(
-    req.body.sentences.map(sentence => {
-      sentence = Sentencer.make(sentence);
-    })
-  ).then(madlib => {
-    res.json(madlib);
-  });
+    //get POS from the body
+    let words = req.body.words;
+    let sentence = req.body.sentence;
+    wordpos.getPOS(words, function(results) {
+        console.log("results from getPOS", results);
+        let adverbsAndVerbs = [];
+
+        if (!results.adverbs.length === 0) {
+            adverbsAndVerbs.push(results.adverbs);
+        }
+        else {
+            adverbsAndVerbs.push(wordpos
+                .randAdverb({
+                    count: 100
+                }));
+        }
+        if (!results.verbs.length === 0) {
+            adverbsAndVerbs.push(results.verbs);
+        }
+        else {
+            adverbsAndVerbs.push(wordpos
+                .randAdverb({
+                    count: 100
+                }));
+        }
+        //Wait until adverbs and verbs have been collected
+        //Also reasonable to have a saved list somewhere
+        Promise.all(adverbsAndVerbs)
+            .then(function([adverbsList, verbsList]) {
+                Sentencer.configure({
+                    nounList: results.nouns,
+                    adjectiveList: results.adjectives,
+
+                    actions: {
+                        adverb: function() {
+                            return _.sample(adverbsList);
+                        },
+                        verb: function() {
+                            return _.sample(verbsList);
+                        }
+                    }
+                });
+                res.json(Sentencer.make(sentence));
+            });
+    });
+
+
+
 });
+
 
 module.exports = router;
--- spec/app_spec.js
+++ spec/app_spec.js
@@ -5,233 +5,329 @@ const User = mongoose.model("User");
 const qs = require("qs");
 
 describe("App", () => {
-  const baseUrl = "http://localhost:8081";
-  const apiUrl = baseUrl + "/api/v1/";
-  let server;
-  let user;
-  const apiUrlFor = (type, params) => {
-    params = params ? `&${qs.stringify(params)}` : "";
-    return `${apiUrl}${type}?token=${user.token}${params}`;
-  };
-  const j = str => JSON.parse(str);
+    const baseUrl = "http://localhost:8081";
+    const apiUrl = baseUrl + "/api/v1/";
+    let server;
+    let user;
+    const apiUrlFor = (type, params) => {
+        params = params ? `&${qs.stringify(params)}` : "";
+        return `${apiUrl}${type}?token=${user.token}${params}`;
+    };
+    const j = str => JSON.parse(str);
 
-  beforeAll(done => {
-    server = app.listen(8081, () => {
-      done();
-    });
-  });
-
-  beforeEach(done => {
-    User.create({
-      fname: "Foo",
-      lname: "Bar",
-      email: "foobar@gmail",
-      password: "password"
-    }).then(result => {
-      user = result;
-      done();
-    });
-  });
-
-  afterAll(done => {
-    server.close();
-    server = null;
-    done();
-  });
-
-  // ----------------------------------------
-  // App
-  // ----------------------------------------
-  it("renders the home page", done => {
-    request.get(baseUrl, (err, res, body) => {
-      expect(res.statusCode).toBe(200);
-      done();
-    });
-  });
-
-  describe("Api call", () => {
-    // ----------------------------------------
-    // POS Endpoints
-    // ----------------------------------------
-    describe("nouns endpoint", () => {
-      it("returns an array", done => {
-        request.get(
-          apiUrlFor("nouns", {
-            count: 10
-          }),
-          (err, res, body) => {
-            let result = j(body);
-            expect(Array.isArray(result)).toBe(true);
+    beforeAll(done => {
+        server = app.listen(8081, () => {
             done();
-          }
-        );
-      });
-
-      it("contains the number of nouns in the count parameter", done => {
-        request.get(
-          apiUrlFor("nouns", {
-            count: 10
-          }),
-          (err, res, body) => {
-            let result = j(body);
-            expect(result.length).toBe(10);
-            done();
-          }
-        );
-      });
-
-      it("contains the number of nouns in the count parameter", done => {
-        request.get(
-          apiUrlFor("nouns", {
-            count: 100
-          }),
-          (err, res, body) => {
-            let result = j(body);
-            expect(result.length).toBe(100);
-            done();
-          }
-        );
-      });
-    });
-    describe("verb endpoint", () => {
-      it("returns an array", done => {
-        request.get(
-          apiUrlFor("verbs", {
-            count: 10
-          }),
-          (err, res, body) => {
-            let result = j(body);
-            expect(Array.isArray(result)).toBe(true);
-            done();
-          }
-        );
-      });
-
-      it("contains the number of verbs in the count parameter", done => {
-        request.get(
-          apiUrlFor("verbs", {
-            count: 10
-          }),
-          (err, res, body) => {
-            let result = j(body);
-            expect(result.length).toBe(10);
-            done();
-          }
-        );
-      });
-
-      it("contains the number of verbs in the count parameter", done => {
-        request.get(
-          apiUrlFor("verbs", {
-            count: 100
-          }),
-          (err, res, body) => {
-            let result = j(body);
-            expect(result.length).toBe(100);
-            done();
-          }
-        );
-      });
-    });
-    describe("adjective endpoint", () => {
-      it("returns an array", done => {
-        request.get(
-          apiUrlFor("adjectives", {
-            count: 10
-          }),
-          (err, res, body) => {
-            let result = j(body);
-            expect(Array.isArray(result)).toBe(true);
-            done();
-          }
-        );
-      });
-
-      it("contains the number of adjectives in the count parameter", done => {
-        request.get(
-          apiUrlFor("adjectives", {
-            count: 10
-          }),
-          (err, res, body) => {
-            let result = j(body);
-            expect(result.length).toBe(10);
-            done();
-          }
-        );
-      });
-
-      it("contains the number of adjectives in the count parameter", done => {
-        request.get(
-          apiUrlFor("adjectives", {
-            count: 100
-          }),
-          (err, res, body) => {
-            let result = j(body);
-            expect(result.length).toBe(100);
-            done();
-          }
-        );
-      });
-    });
-    describe("adverb endpoint", () => {
-      it("returns an array", done => {
-        request.get(
-          apiUrlFor("adverbs", {
-            count: 10
-          }),
-          (err, res, body) => {
-            let result = j(body);
-            expect(Array.isArray(result)).toBe(true);
-            done();
-          }
-        );
-      });
-
-      it("contains the number of adverbs in the count parameter", done => {
-        request.get(
-          apiUrlFor("adverbs", {
-            count: 10
-          }),
-          (err, res, body) => {
-            let result = j(body);
-            expect(result.length).toBe(10);
-            done();
-          }
-        );
-      });
-
-      it("contains the number of adverbs in the count parameter", done => {
-        request.get(
-          apiUrlFor("adverbs", {
-            count: 100
-          }),
-          (err, res, body) => {
-            let result = j(body);
-            expect(result.length).toBe(100);
-            done();
-          }
-        );
-      });
-    });
-    describe("madlibs endpoint", () => {
-      it("had a baby", done => {
-        let formData = {
-          nouns: [1, 2, 3, 4],
-          adjectives: [1, 2, 3, 4],
-          verbs: [1, 2, 3, 4],
-          adverbs: [1, 2, 3, 4],
-          sentences: [
-            "This sentence has {{ a_noun }} and {{ an_adjective }} {{ noun }} in it.",
-            "This sentence has {{ a_noun }} and {{ adverb }} {{ noun }} in it.",
-            "This sentence has {{ verb }} and {{ verb }} {{ noun }} in it."
-          ]
-        };
-        request.post(apiUrlFor("madlibs"), (err, res, formData) => {
-          console.log(res.body);
-          expect(res.length).toBe(3);
-          done();
         });
-      });
     });
-  });
+
+    beforeEach(done => {
+        User.create({
+            fname: "Foo",
+            lname: "Bar",
+            email: "foobar@gmail",
+            password: "password"
+        }).then(result => {
+            user = result;
+            done();
+        });
+    });
+
+    afterAll(done => {
+        server.close();
+        server = null;
+        done();
+    });
+
+    // ----------------------------------------
+    // App
+    // ----------------------------------------
+    it("renders the home page", done => {
+        request.get(baseUrl, (err, res, body) => {
+            expect(res.statusCode).toBe(200);
+            done();
+        });
+    });
+
+    describe("Api call", () => {
+        // ----------------------------------------
+        // POS Endpoints
+        // ----------------------------------------
+        describe("nouns endpoint", () => {
+            it("returns an array", done => {
+                request.get(
+                    apiUrlFor("nouns", {
+                        count: 10
+                    }),
+                    (err, res, body) => {
+                        let result = j(body);
+                        expect(Array.isArray(result)).toBe(true);
+                        done();
+                    }
+                );
+            });
+
+            it("contains the number of nouns in the count parameter", done => {
+                request.get(
+                    apiUrlFor("nouns", {
+                        count: 10
+                    }),
+                    (err, res, body) => {
+                        let result = j(body);
+                        expect(result.length).toBe(10);
+                        done();
+                    }
+                );
+            });
+
+            it("contains the number of nouns in the count parameter", done => {
+                request.get(
+                    apiUrlFor("nouns", {
+                        count: 100
+                    }),
+                    (err, res, body) => {
+                        let result = j(body);
+                        expect(result.length).toBe(100);
+                        done();
+                    }
+                );
+            });
+        });
+        describe("verb endpoint", () => {
+            it("returns an array", done => {
+                request.get(
+                    apiUrlFor("verbs", {
+                        count: 10
+                    }),
+                    (err, res, body) => {
+                        let result = j(body);
+                        expect(Array.isArray(result)).toBe(true);
+                        done();
+                    }
+                );
+            });
+
+            it("contains the number of verbs in the count parameter", done => {
+                request.get(
+                    apiUrlFor("verbs", {
+                        count: 10
+                    }),
+                    (err, res, body) => {
+                        let result = j(body);
+                        expect(result.length).toBe(10);
+                        done();
+                    }
+                );
+            });
+
+            it("contains the number of verbs in the count parameter", done => {
+                request.get(
+                    apiUrlFor("verbs", {
+                        count: 100
+                    }),
+                    (err, res, body) => {
+                        let result = j(body);
+                        expect(result.length).toBe(100);
+                        done();
+                    }
+                );
+            });
+        });
+        describe("adjective endpoint", () => {
+            it("returns an array", done => {
+                request.get(
+                    apiUrlFor("adjectives", {
+                        count: 10
+                    }),
+                    (err, res, body) => {
+                        let result = j(body);
+                        expect(Array.isArray(result)).toBe(true);
+                        done();
+                    }
+                );
+            });
+
+            it("contains the number of adjectives in the count parameter", done => {
+                request.get(
+                    apiUrlFor("adjectives", {
+                        count: 10
+                    }),
+                    (err, res, body) => {
+                        let result = j(body);
+                        expect(result.length).toBe(10);
+                        done();
+                    }
+                );
+            });
+
+            it("contains the number of adjectives in the count parameter", done => {
+                request.get(
+                    apiUrlFor("adjectives", {
+                        count: 100
+                    }),
+                    (err, res, body) => {
+                        let result = j(body);
+                        expect(result.length).toBe(100);
+                        done();
+                    }
+                );
+            });
+        });
+        describe("adverb endpoint", () => {
+            it("returns an array", done => {
+                request.get(
+                    apiUrlFor("adverbs", {
+                        count: 10
+                    }),
+                    (err, res, body) => {
+                        let result = j(body);
+                        expect(Array.isArray(result)).toBe(true);
+                        done();
+                    }
+                );
+            });
+
+            it("contains the number of adverbs in the count parameter", done => {
+                request.get(
+                    apiUrlFor("adverbs", {
+                        count: 10
+                    }),
+                    (err, res, body) => {
+                        let result = j(body);
+                        expect(result.length).toBe(10);
+                        done();
+                    }
+                );
+            });
+
+            it("contains the number of adverbs in the count parameter", done => {
+                request.get(
+                    apiUrlFor("adverbs", {
+                        count: 100
+                    }),
+                    (err, res, body) => {
+                        let result = j(body);
+                        expect(result.length).toBe(100);
+                        done();
+                    }
+                );
+            });
+        });
+        describe("madlibs endpoint", () => {
+            it("returns sentences", done => {
+                let form = {
+                    words: "dog",
+                    sentence: "The {{ noun }}."
+                }
+                request.post({
+                        url: apiUrlFor("madlibs"),
+                        form: form
+                    }, function(err, httpResponse, body) {
+                        expect(httpResponse.statusCode).toBe(200);
+                        let result = j(body);
+                        expect(result).toEqual("The dog.");
+                        done();
+                    }
+                );
+            });
+            it("returns sentences", done => {
+                let form = {
+                    words: "bunny",
+                    sentence: "The {{ noun }}."
+                }
+                request.post({
+                        url: apiUrlFor("madlibs"),
+                        form: form
+                    }, function(err, httpResponse, body) {
+                        expect(httpResponse.statusCode).toBe(200);
+                        let result = j(body);
+                        expect(result).toEqual("The bunny.");
+                        done();
+                    }
+                );
+            });
+            it("returns sentences with noun and verb", done => {
+                let form = {
+                    words: "bunny sing",
+                    sentence: "The {{ noun }} {{ verb }}."
+                }
+                request.post({
+                        url: apiUrlFor("madlibs"),
+                        form: form
+                    }, function(err, httpResponse, body) {
+                        expect(httpResponse.statusCode).toBe(200);
+                        let result = j(body);
+                        expect(result).toEqual("The bunny sing.");
+                        done();
+                    }
+                );
+            });
+            it("returns sentences with noun, verb, adjective", done => {
+                let form = {
+                    words: "bunny sing different",
+                    sentence: "The {{adjective}} {{ noun }} {{ verb }}."
+                }
+                request.post({
+                        url: apiUrlFor("madlibs"),
+                        form: form
+                    }, function(err, httpResponse, body) {
+                        expect(httpResponse.statusCode).toBe(200);
+                        let result = j(body);
+                        expect(result).toEqual("The different bunny sing.");
+                        done();
+                    }
+                );
+            });
+            it("returns sentences with noun, verb, adjective, adverb", done => {
+                let form = {
+                    words: "bunny sing different calmly",
+                    sentence: "The {{adjective}} {{ noun }} {{ verb }} {{adverb}}."
+                }
+                request.post({
+                        url: apiUrlFor("madlibs"),
+                        form: form
+                    }, function(err, httpResponse, body) {
+                        expect(httpResponse.statusCode).toBe(200);
+                        let result = j(body);
+                        expect(result).toEqual("The different bunny sing calmly.");
+                        done();
+                    }
+                );
+            });
+            xit("returns sentences with noun, verb, adjective and default adverb", done => {
+                let form = {
+                    words: "bunny sing different",
+                    sentence: "The {{adjective}} {{ noun }} {{ verb }} {{adverb}}."
+                }
+                request.post({
+                        url: apiUrlFor("madlibs"),
+                        form: form
+                    }, function(err, httpResponse, body) {
+                        expect(httpResponse.statusCode).toBe(200);
+                        let result = j(body);
+                        let regexp = /The different bunny sing [^undefined].*\./;
+                        expect(regexp.test(result)).toBe(true);
+                        done();
+                    }
+                );
+            });
+            xit("returns sentences with noun, adjective, adverb and default verb", done => {
+                let form = {
+                    words: "bunny different calmly",
+                    sentence: "The {{adjective}} {{ noun }} {{ verb }} {{adverb}}."
+                }
+                request.post({
+                        url: apiUrlFor("madlibs"),
+                        form: form
+                    }, function(err, httpResponse, body) {
+                        expect(httpResponse.statusCode).toBe(200);
+                        let result = j(body);
+                        let regexp = /The different bunny [^undefined].* calmly\./;
+                        expect(regexp.test(result)).toBe(true);
+                        done();
+                    }
+                );
+            });
+        });
+    });
 });
